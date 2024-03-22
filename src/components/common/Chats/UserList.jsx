e380355b6631399e466540aac92019bc124481d3
amirabas <abasaarafi@outlook.com>
2024-03-22 09:48:26 +0330

    add name link menu

--- src/components/common/Chats/UserList.jsx
+++ src/components/common/Chats/UserList.jsx
@@ -1,3 +1,4 @@
+// Chats.jsx
 import { CircleCheck } from "../../../constants/icons"
 
 
@@ -5,7 +6,7 @@ import { CircleCheck } from "../../../constants/icons"
 function UserList() {
   return (
     <div className="flex gap-3 mt-4 p-2 cursor-pointer border-b-2 border-gray-300">
-        <img src="/avatar.webp" alt="not-found" width={30}/>
+        <img src="/avatar.webp" alt="not-found" className="w-10s h-10 mt-3"/>
         <div className="flex-col ">
               <h3 className="font-bold text-sm tracking-tight my-1">امیر رضا جلالی 
                   <span><CircleCheck className="text-red-600 inline text-xl mr-10" /></span>
