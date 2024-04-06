28c7b81da7920986c669bf19d8d6b19d1e414039
amirabas <abasaarafi@outlook.com>
2024-04-06 11:50:17 +0330

    change card ui

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,18 +1,30 @@
+
+import react from 'react'
 import {
     Card, CardHeader, CardBody, CardFooter, Heading, Button, Text, Flex, Table,
 } from '@chakra-ui/react'
+import { FaShoppingBasket } from "react-icons/fa";
+import { FaArrowTrendDown } from "react-icons/fa6";
+import { FaArrowTrendUp } from "react-icons/fa6";
 
 function CardeCom({ title, caption, button, img }) {
     return (
-        <Card>
+        <Card display="flex">
             <CardHeader>
-                <Heading size='md'>{title}</Heading>
+                <Heading size='md'>
+                    <FaShoppingBasket className='text-5xl p-3 bg-blue-700 rounded-full
+                    text-white
+                    ' />
+                </Heading>
             </CardHeader>
             <CardBody>
-                <Text>{caption}</Text>
+                <Text>خرید /
+                    <span className="font-bold m-4 text-2xl">28947</span>
+                </Text>
             </CardBody>
-            <CardFooter>
-                <Button>{button}</Button>
+            <CardFooter display="flex" gap="10px">
+                <FaArrowTrendDown className='text-2xl font-light inline-flex' />
+                <h4>3% / سال قبل</h4>
             </CardFooter>
         </Card>
     )
